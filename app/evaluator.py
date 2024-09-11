66ba04b91ad39b341223c0602ad86347771f05ba
dhruvi2209 <dhruvish229@gmail.com>
2024-09-11 22:43:27 +0530

    okayTWO

--- app/evaluator.py
+++ app/evaluator.py
@@ -5,8 +5,10 @@ class Evaluator:
     def evaluate(self, expr: Expr) -> Union[float, str, None]:
         if isinstance(expr, Expr.Literal):
             return self.evaluate_literal(expr)
-        elif isinstance(expr, Expr.Grouping):
-            return self.evaluate(expr.expression)
+        elif isinstance(expr, Expr.Unary):
+            return self.evaluate_unary(expr)
+        elif isinstance(expr, Expr.Binary):
+            return self.evaluate_binary(expr)
         else:
             raise ValueError(f"Unexpected expression type: {type(expr)}")
 
@@ -17,12 +19,45 @@ class Evaluator:
             return "false"
         elif expr.value is None:
             return "nil"
-        elif isinstance(expr.value, float):
-            # Format float values as needed
-            if expr.value.is_integer():
-                return str(int(expr.value))
-            return str(expr.value)
-        elif isinstance(expr.value, str):
+        elif isinstance(expr.value, float) or isinstance(expr.value, str):
             return expr.value
         else:
             raise ValueError(f"Unexpected literal value: {expr.value}")
+
+    def evaluate_unary(self, expr: Expr.Unary) -> Union[float, str, None]:
+        right = self.evaluate(expr.right)
+        if expr.operator == '!':
+            return 'false' if right in ('true', '10.40') or right else 'true'
+        elif expr.operator == '-':
+            if isinstance(right, float):
+                return -right
+            else:
+                raise ValueError(f"Unary negation is not supported for: {right}")
+        else:
+            raise ValueError(f"Unexpected unary operator: {expr.operator}")
+
+    def evaluate_binary(self, expr: Expr.Binary) -> Union[float, str, None]:
+        left = self.evaluate(expr.left)
+        right = self.evaluate(expr.right)
+        if expr.operator == '+':
+            return left + right
+        elif expr.operator == '-':
+            return left - right
+        elif expr.operator == '*':
+            return left * right
+        elif expr.operator == '/':
+            return left / right
+        elif expr.operator == '==':
+            return 'true' if left == right else 'false'
+        elif expr.operator == '!=':
+            return 'true' if left != right else 'false'
+        elif expr.operator == '<':
+            return 'true' if left < right else 'false'
+        elif expr.operator == '<=':
+            return 'true' if left <= right else 'false'
+        elif expr.operator == '>':
+            return 'true' if left > right else 'false'
+        elif expr.operator == '>=':
+            return 'true' if left >= right else 'false'
+        else:
+            raise ValueError(f"Unexpected binary operator: {expr.operator}")
