6648f789075a104d21f4b610ee79c5974a5ad800
dhruvi2209 <dhruvish229@gmail.com>
2024-09-11 21:08:42 +0530

    Evaluate Three

--- app/evaluator.py
+++ app/evaluator.py
@@ -6,16 +6,16 @@ from my_parser import Expr
 
 
 class Evaluator:
-    def evaluate(self, expr: Expr) -> Union[bool, None]:
+    def evaluate(self, expr: Expr) -> Union[str, None]:
         if isinstance(expr, Expr.Literal):
             return self.evaluate_literal(expr)
         raise ValueError(f"Unexpected expression type: {type(expr)}")
 
-    def evaluate_literal(self, expr: Expr.Literal) -> Union[bool, None]:
+    def evaluate_literal(self, expr: Expr.Literal) -> Union[str, None]:
         if expr.value is True:
-            return True
+            return "true"
         elif expr.value is False:
-            return False
+            return "false"
         elif expr.value is None:
-            return None
+            return "nil"
         raise ValueError(f"Unexpected literal value: {expr.value}")
