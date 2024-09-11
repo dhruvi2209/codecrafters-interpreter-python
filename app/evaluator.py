2013d6038de2cdfd652cd2bc25e092d668bf67b1
dhruvi2209 <dhruvish229@gmail.com>
2024-09-11 22:34:55 +0530

    okaydokey

--- app/evaluator.py
+++ app/evaluator.py
@@ -1,11 +1,12 @@
 from typing import Union
 from my_parser import Expr
-from lox import Lox
 
 class Evaluator:
     def evaluate(self, expr: Expr) -> Union[float, str, None]:
         if isinstance(expr, Expr.Literal):
             return self.evaluate_literal(expr)
+        elif isinstance(expr, Expr.Grouping):
+            return self.evaluate(expr.expression)
         else:
             raise ValueError(f"Unexpected expression type: {type(expr)}")
 
@@ -17,11 +18,10 @@ class Evaluator:
         elif expr.value is None:
             return "nil"
         elif isinstance(expr.value, float):
-            # Print float values without trailing ".0" if they are whole numbers
+            # Format float values as needed
             if expr.value.is_integer():
-                return int(expr.value)
-            else:
-                return expr.value
+                return str(int(expr.value))
+            return str(expr.value)
         elif isinstance(expr.value, str):
             return expr.value
         else:
