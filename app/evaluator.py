45937bdb609233c3218407023d13fe7abe91e1d7
dhruvi2209 <dhruvish229@gmail.com>
2024-09-12 22:06:57 +0530

    acha

--- app/evaluator.py
+++ app/evaluator.py
@@ -27,41 +27,37 @@ class Evaluator:
         right = self.evaluate(expr.right)
 
         if expr.operator.type == TokenType.PLUS:
-            if isinstance(left, str) and isinstance(right, str):
-                return left + right
-            elif isinstance(left, (Decimal, int)) and isinstance(right, (Decimal, int)):
-                return Decimal(left) + Decimal(right)
-            else:
-                raise RuntimeError(expr.operator, "Operands must be two numbers or two strings.")
-        
+            self.__checkStringOrNumberOperands(left, right)
+            return left + right
+            
         elif expr.operator.type == TokenType.MINUS:
-            if isinstance(left, (Decimal, int)) and isinstance(right, (Decimal, int)):
-                return Decimal(left) - Decimal(right)
-            else:
-                raise RuntimeError(expr.operator, "Operands must be numbers.")
-        
+            self.__checkNumberOperands(left, right)
+            return Decimal(left) - Decimal(right)
+            
         elif expr.operator.type == TokenType.STAR:
-            if isinstance(left, (Decimal, int)) and isinstance(right, (Decimal, int)):
-                return Decimal(left) * Decimal(right)
-            else:
-                raise RuntimeError(expr.operator, "Operands must be numbers.")
-        
+            self.__checkNumberOperands(left, right)
+            return Decimal(left) * Decimal(right)
+            
         elif expr.operator.type == TokenType.SLASH:
-            if isinstance(left, (Decimal, int)) and isinstance(right, (Decimal, int)):
-                if right == 0:
-                    raise RuntimeError(expr.operator, "Division by zero is not allowed.")
-                return Decimal(left) / Decimal(right)
-            else:
-                raise RuntimeError(expr.operator, "Operands must be numbers.")
-        
+            self.__checkNumberOperands(left, right)
+            if right == 0:
+                raise RuntimeError(expr.operator, "Division by zero is not allowed.")
+            return Decimal(left) / Decimal(right)
+            
         elif expr.operator.type in {
             TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, 
             TokenType.LESS_EQUAL, TokenType.EQUAL_EQUAL, TokenType.BANG_EQUAL
         }:
             return self.evaluate_comparison(expr.operator, left, right)
-        
+            
         else:
             raise RuntimeError(expr.operator, f"Unexpected binary operator: {expr.operator}")
+
+    def __checkStringOrNumberOperands(self, left, right):
+        if not ((isinstance(left, str) and isinstance(right, str)) or 
+                (isinstance(left, (Decimal, int)) and isinstance(right, (Decimal, int)))):
+            raise RuntimeError("Operands must be two numbers or two strings.")
+
 
     def runtime_error(self, error: RuntimeError) -> None:
         print(f"{error}\n[line {error.token.line}]", file=sys.stderr)
