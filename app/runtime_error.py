58c6c927aa8839383257f87c1ab4ed16388c4a53
dhruvi2209 <dhruvish229@gmail.com>
2024-09-12 16:39:22 +0530

    Hojaokyu

--- app/runtime_error.py
+++ app/runtime_error.py
@@ -1,4 +1,7 @@
 class RuntimeError(Exception):
-    def __init__(self, operator, message):
+    def __init__(self, token, message):
         super().__init__(message)
-        self.operator = operator
+        self.token = token  # Token provides the line number and other details
+
+    def __str__(self):
+        return f"[line {self.token.line}] Error: {self.message}"
